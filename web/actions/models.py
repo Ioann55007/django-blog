30ff7a2a74fd4842666e39d5d3a117f8f53eabad
Ioann <ioan@mail.cru>
2021-10-10 19:10:06 +0500

    follower

--- web/actions/models.py
+++ web/actions/models.py
@@ -1,9 +1,17 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Follower(models.Model):
     """Модель подписчиков"""
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='own')
-    subscriber = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='follower')
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rel_from')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rel_to')
+    created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created',)
+        unique_together = ('subscriber', 'to_user')
